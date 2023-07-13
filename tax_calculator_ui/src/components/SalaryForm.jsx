dba16a49a436dbffc72957c39fb039e994e5fa45
aabhishaek <abhishekcs007@gmail.com>
2023-07-13 23:15:16 +0530

    Add barebones salary form component to get the salary details

---
+++ tax_calculator_ui/src/components/SalaryForm.jsx
@@ -0,0 +1,32 @@
+function SalaryForm() {
+  return (
+    <form>
+      <div>
+        <label>Basic Salary</label>
+        <input type="number" />
+      </div>
+      <div>
+        <label>House Rent Allowance</label>
+        <input type="number" />
+      </div>
+      <div>
+        <label>Special Allowance</label>
+        <input type="number" />
+      </div>
+      <div>
+        <label>Basic Salary</label>
+        <input type="number" />
+      </div>
+      <div>
+        <label>Basic Salary</label>
+        <input type="number" />
+      </div>
+      <div>
+        <label>Basic Salary</label>
+        <input type="number" />
+      </div>
+    </form>
+  );
+}
+
+export default SalaryForm;
