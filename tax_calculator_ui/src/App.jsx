e4c1254ad9c77df31e5f5e8ae69d102190743a10
aabhishaek <abhishekcs007@gmail.com>
2023-07-13 23:15:50 +0530

    Add salary form component to app.jsx -- tobe refactored later with routing

--- tax_calculator_ui/src/App.jsx
+++ tax_calculator_ui/src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
-import MainNavigation from './components/MainNavigation'
+import "./App.css";
+import MainNavigation from "./components/MainNavigation";
+import SalaryForm from "./components/SalaryForm";
 
 function App() {
-  return <>
-  <MainNavigation />
-  </>
+  return (
+    <>
+      <MainNavigation />
+      <SalaryForm />
+    </>
+  );
 }
 
-export default App
+export default App;
